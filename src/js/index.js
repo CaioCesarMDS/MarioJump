61c20f25e65aa65d0361539e59c863ebcbdcb803
CaioCesarMDS <caiocesarsts@gmail.com>
2024-05-10 12:09:17 -0300

    feat: version 1.0

--- src/js/index.js
+++ src/js/index.js
@@ -1,61 +1,73 @@
-const mario = document.querySelector(".mario");
-const pipe = document.querySelector(".pipe");
-const gameOverImg = document.querySelector(".game-over");
-const warning = document.querySelector("p");
+function main() {
+    const mario = document.querySelector(".mario");
+    const pipe = document.querySelector(".pipe");
+    const finalGameOver = document.querySelector(".game-over");
+    let gameOverFlag = false;
 
-document.addEventListener("keydown", jump);
+    document.addEventListener("keydown", jump);
 
-function jump(event) {
-    if (event.key === " ") {
+    function jump(event) {
+        if (event.key === " ") {
+            mario.classList.add("jump");
+            setTimeout(() => {
+                mario.classList.remove("jump");
+            }, 500);
+        }
+    }
+
+    const loop = setInterval(() => {
+        const pipePosition = pipe.offsetLeft;
+        const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
+        if (
+            !gameOverFlag &&
+            pipePosition <= 120 &&
+            pipePosition > 0 &&
+            marioPosition < 99
+        ) {
+            gameOverFlag = true;
+            gameOver(pipePosition, marioPosition);
+        }
+    }, 10);
+
+    function gameOver(pipePosition, marioPosition) {
+        pipe.style.animation = "none";
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.classList.remove("jump");
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = "/src/images/game-over.png";
+        mario.style.width = "70px";
+        mario.style.marginLeft = "50px";
+
+        finalGameOver.style.display = "flex";
+
+        setTimeout(() => {
+            document.addEventListener("keydown", restartGameListener);
+        }, 3000);
+    }
+
+    function restartGameListener(event) {
+        if (gameOverFlag && event.key === " ") {
+            document.removeEventListener("keydown", restartGameListener);
+            restartGame();
+        }
+    }
+
+    function restartGame() {
+        console.log("ola");
+        mario.src = "/src/images/mario.gif";
+        mario.style.width = "150px";
+        mario.style.marginLeft = "0";
+        mario.style.bottom = "0";
         mario.classList.add("jump");
-        setTimeout(() => {
-            mario.classList.remove("jump");
-        }, 500);
+
+        pipe.style.animation = "walkPipe 2s infinite linear";
+        pipe.style.left = null;
+
+        finalGameOver.style.display = "none";
+        gameOverFlag = false;
     }
 }
 
-const loop = setInterval(() => {
-    const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
-    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 99) {
-        gameOver(pipePosition, marioPosition);
-        setTimeout(() => {
-            document.addEventListener("keydown", (event) => {
-                if (event.key === " ") {
-                    restartGame();
-                }
-            });
-        }, 2000);
-    }
-}, 10);
-
-function gameOver(pipePosition,marioPosition) {
-    pipe.style.animation = "none";
-    pipe.style.left = `${pipePosition}px`;
-
-    mario.style.animation = "none";
-    mario.style.bottom = `${marioPosition}px`;
-
-    mario.src = "/src/images/game-over.png";
-    mario.style.width = "70px";
-    mario.style.marginLeft = "50px";
-
-    gameOverImg.style.display = "block";
-    warning.style.display = "block";
-}
-
-function restartGame() {
-    mario.src = "/src/images/mario.gif";
-    mario.style.width = "150px";
-    mario.style.marginLeft = "0";
-    mario.style.bottom = "0";
-
-    pipe.style.animation = "walkPipe 2s infinite linear";
-    pipe.style.left = null;
-
-    gameOverImg.style.display = "none";
-    warning.style.display = "none";
-    mario.style.animation = "marioJump .5s ease-out";
-}
-
-document.addEventListener("keydown", jump);
+main();
