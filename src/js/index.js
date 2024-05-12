e0eb2b88b1778d8044ca240c3744cdec4c7688bd
CaioCesarMDS <caiocesarsts@gmail.com>
2024-05-12 15:50:44 -0300

    feat: add music and sound effects

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,22 @@
 function main(get) {
     const mainMenu = document.querySelector(".main-menu");
+
     const mario = document.querySelector(".mario");
     const pipe = document.querySelector(".pipe");
     const clouds = document.querySelector(".clouds");
     const ground = document.querySelector(".ground");
+
     const gamerOverEl = document.querySelector(".game-over");
-    const bestScoreEl = document.querySelector(".best-score");
     const pause = document.querySelector(".pause");
+
+    const bestScoreEl = document.querySelector(".best-score");
     const scoreEl = document.getElementById("score");
     const timeEl = document.getElementById("time");
+
+    const music = document.getElementById("background-music");
+    const audioJump = document.getElementById("audio-jump");
+    const gameOverAudio = document.getElementById("game-over-audio");
+    const punctuationMarkAudio = document.getElementById("punctuation-mark-audio");
 
     let scoreRecord = 0;
     let bestScore = 0;
@@ -19,7 +27,6 @@ function main(get) {
     let timeCount;
     let score = 0;
     let time = 0;
-    let count = 0;
 
     gameSetup();
 
@@ -31,6 +38,13 @@ function main(get) {
         pipe.style.animationPlayState = "paused";
         clouds.style.animationPlayState = "paused";
         ground.style.animationPlayState = "paused";
+        volumeAdjustment();
+
+        function volumeAdjustment() {
+            music.volume = 0.4;
+            audioJump.volume = 0.7;
+            gameOverAudio.volume = 0.6;
+        }
     }
 
     setTimeout(() => {
@@ -57,6 +71,7 @@ function main(get) {
         function jump(event) {
             if (event.keyCode === 32 && mainMenu.style.display === "none") {
                 mario.classList.add("jump");
+                audioJump.play();
                 setTimeout(() => {
                     mario.classList.remove("jump");
                 }, 500);
@@ -84,6 +99,9 @@ function main(get) {
             scoreCount = setInterval(() => {
                 score += 1;
                 scoreEl.textContent = `0${score}`;
+                if (score % 500 === 0) {
+                    punctuationMarkAudio.play();
+                }
             }, 100);
 
             timeCount = setInterval(() => {
@@ -147,6 +165,10 @@ function main(get) {
         function gameOver(pipePosition, marioPosition) {
             saveScoreRecord();
             resetCount();
+
+            music.pause();
+            gameOverAudio.play();
+
             pipe.style.animation = "none";
             pipe.style.left = `${pipePosition}px`;
 
@@ -169,7 +191,9 @@ function main(get) {
         }
 
         function restartGame(event) {
+            gameOverAudio.load();
             if (gameOverFlag && event.keyCode === 32) {
+                music.play();
                 mario.src = "/src/images/mario.gif";
                 mario.style.width = "150px";
                 mario.style.marginLeft = "0";
@@ -225,7 +249,22 @@ document.addEventListener("click", (e) => {
     const loadScoreBtn = document.querySelector(".load-score");
     const returnMenu = document.querySelector(".return-menu-btn");
     const timer = document.querySelector(".timer");
+    const music = document.getElementById("background-music");
+    const audio = document.getElementById("audio-on-off");
+
     let count = 0;
+
+    if (target === audio) {
+        if (audio.children[0].src === "/src/images/audio-off.svg") {
+            audio.children[0].src = "/src/images/audio-on.svg";
+            music.muted = false;
+            music.volume = 0.8;
+            music.play();
+        } else {
+            audio.children[0].src = "/src/images/audio-off.svg";
+            music.pause();
+        }
+    }
 
     if (target === newGameBtn) {
         mainMenu.style.display = "none";
@@ -261,5 +300,6 @@ document.addEventListener("click", (e) => {
 
     if (target === returnMenu) {
         mainMenu.style.display = "flex";
+        music.play();
     }
 });
